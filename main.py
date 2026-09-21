e3c58deb061042af971ef7619f73cf2fdc195b30
Melody.. 🌎 <codexx.agent@gmail.com>
2026-09-21 07:16:51 +0530

    Change Uvicorn port from 8000 to 1000

--- main.py
+++ main.py
@@ -287,4 +287,4 @@ def search_number(user_number: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=1000)
